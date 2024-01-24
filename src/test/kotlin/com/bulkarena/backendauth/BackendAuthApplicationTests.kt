91d251d476bdeb7cc53fba0c6988c9643e14e5a5
Harsh3305 <harshverma3305@gmail.com>
2024-01-24 23:30:25 +0530

    Initialise empty project

---
+++ src/test/kotlin/com/bulkarena/backendauth/BackendAuthApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.bulkarena.backendauth
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BackendAuthApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
